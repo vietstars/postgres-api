da15b853dc93efb9e39414b9c6d0373f1d4cb000
Việt Stars <vietstar.nt@gmail.com>
2023-05-25 13:08:34 +0700

    Add files via upload

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+  "log"
+  "net/http"
+  "os"
+  "time"
+
+  "github.com/joho/godotenv"
+  "github.com/vietstars/postgres-api/controllers"
+  "github.com/vietstars/postgres-api/models"
+)
+
+const (
+  defaultPort = "8008"
+  idleTimeout       = 30 * time.Second
+  writeTimeout      = 180 * time.Second
+  readHeaderTimeout = 10 * time.Second
+  readTimeout       = 10 * time.Second
+)
+
+func main() {
+  err := godotenv.Load()
+  check(err)
+
+  port := os.Getenv("PORT") 
+  if port == "" {
+    port = defaultPort
+  }
+
+  handler := controllers.New() 
+
+  server := &http.Server{
+    Addr:    "0.0.0.0:" + port,
+    Handler: handler,
+
+    IdleTimeout:       idleTimeout,
+    WriteTimeout:      writeTimeout,
+    ReadHeaderTimeout: readHeaderTimeout,
+    ReadTimeout:       readTimeout,
+  }
+
+  models.ConnectDatabase()
+
+  err = server.ListenAndServe()
+  check(err)
+}
+
+func check(e error){
+  if e != nil {
+    log.Fatal(e)
+  }
+}
+
