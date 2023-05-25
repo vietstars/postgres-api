7148efd939f93d4c044019d3fdcd2e27adec16d2
Việt Stars <vietstar.nt@gmail.com>
2023-05-25 12:55:37 +0700

    Add files via upload

---
+++ models/quest.go
@@ -0,0 +1,27 @@
+package models
+
+import "time"
+
+type Quest struct {
+  ID uint `json:"id" gorm:"primary_key"`  
+  Title string `json:"title"`
+  Description string `json:"description"`
+  Reward int `json:"reward"`
+  CategoryID uint `json:"category_id"`
+  CreatedAt time.Time `json:"created_at"`
+  UpdatedAt time.Time `json:"updated_at"`
+}
+
+func NewQuest(title string, description string, reward int) (quest *Quest, err error){
+  quest = &Quest{
+    Title: title,
+    Description: description,
+    Reward: reward,
+  }
+
+  DB.Create(&quest)
+   
+  return quest, nil
+}
+
+
